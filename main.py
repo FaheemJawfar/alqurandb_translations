8ac5563fc54f2c4b93ea2297a36e33ee57a03dc8
Faheem <faheemjawfar@gmail.com>
2023-12-22 08:24:03 +0530

    Script update to escape quot and follow intended

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import os
 import xml.etree.ElementTree as ET
 
+
+def prettify(element, indent='    '):
+    """Function to add indentation and newlines to make the XML file more readable."""
+    queue = [(0, element)]  # (level, element)
+    while queue:
+        level, element = queue.pop(0)
+        children = list(element)
+        if children:
+            element.text = '\n' + indent * (level + 1)  # for child open
+        if level > 0:
+            element.tail = '\n' + indent * level  # for parent close
+        for child in children:
+            queue.append((level + 1, child))
+        if not children:
+            element.tail = '\n' + indent * level
+
+
+def replace_characters(text):
+    """Function to replace specific characters in a string."""
+    if text:
+        return text.replace('“', '"').replace('”', '"').replace('‘', "'").replace('’', "'")
+    return text
+
+
 # Input and output folders
 input_folder = "source_data/xml"
 output_folder = "modified_version"
@@ -35,13 +59,17 @@ for filename in os.listdir(input_folder):
             # Iterate through aya elements within each sura
             for aya in sura.findall('.//aya'):
                 aya_number = aya.get('index')
-                aya_text = aya.get('text')
+                aya_text = replace_characters(aya.get('text'))
 
                 # Create a new aya element in the new XML structure
-                new_aya = ET.SubElement(new_sura, 'aya', index=str(current_index), aya_number=aya_number, text=aya_text, footnotes='')
+                new_aya = ET.SubElement(new_sura, 'aya', index=str(current_index), aya_number=aya_number, text=aya_text,
+                                        footnotes='')
 
                 # Increment the current index
                 current_index += 1
+
+        # Prettify the XML
+        prettify(new_root)
 
         # Construct the full path for the output file
         output_file_path = os.path.join(output_folder, filename)
